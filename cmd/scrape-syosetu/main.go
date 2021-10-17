44489792a5056a82e77f3b7a98e43b91401cc46a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-10-17 14:48:44 +0900

    fetch html from syosetu

--- cmd/scrape-syosetu/main.go
+++ cmd/scrape-syosetu/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -9,10 +11,10 @@ import (
 func main() {
 	series := "n6316bn"
 	start := 1
-	end := 304 // 304
+	end := 2 // 304
 
 	chapters := downloadChapters(series, start, end)
-	WriteChapters(chapters, "./out/"+series)
+	_ = WriteChapters(chapters, "./out/"+series)
 }
 
 type Chapter struct {
@@ -25,19 +27,38 @@ func downloadChapters(series string, start, end int) []Chapter {
 	wg := &sync.WaitGroup{}
 
 	for i := start; i <= end; i++ {
+		wg.Add(1)
+
 		if i%10 == 0 {
 			time.Sleep(1 * time.Second)
 		}
 
 		go func(idx int) {
-			wg.Add(1)
 			url := urlFor(series, idx)
-			fmt.Println(url)
+			fmt.Println("fetching " + url)
+			body := fetch(url)
+			fmt.Println(body)
 			wg.Done()
 		}(i)
 	}
 
+	wg.Wait()
+
 	return nil
+}
+
+func fetch(url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
 }
 
 func urlFor(series string, chapterIndex int) string {
