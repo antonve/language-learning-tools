ef92b09cd6e9d9ee8cd936ec4eef35065c9bebd0
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-10-18 14:30:12 +0900

    add basic sidebar

--- frontend/anki/src/app/components/Layout.tsx
+++ frontend/anki/src/app/components/Layout.tsx
@@ -4,7 +4,7 @@ import Button from '@app/components/Button'
 const Layout: FC<{}> = ({children}) => (
   <div>
     <header
-      className={`bg-grey-lightest border-t-4 border-purple-400 hero-background p-10`}
+      className={`bg-grey-lightest border-t-4 border-purple-400 hero-background p-10 flex justify-between`}
     >
       <h1 className="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
         Anki Miner
--- frontend/anki/src/pages/index.tsx
+++ frontend/anki/src/pages/index.tsx
@@ -1,8 +1,72 @@
 import type { NextPage } from 'next'
+import classNames from 'classnames'
 import Layout from '@app/components/Layout'
 
 const Home: NextPage = () => {
-  return <Layout>test</Layout>
+  return (
+    <Layout>
+      <div className="flex">
+        <div className="h-12 w-full md:w-1/4">
+          <Sidebar activeId={3} />
+        </div>
+        <div className="bg-gray-50 h-12 w-full md:w-3/4 rounded-sm">
+          <CardWizard />
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Home
+
+const Sidebar = ({ activeId }: { activeId: number }) => {
+  const words = [
+    { id: 1, value: '手が回らない', done: true },
+    { id: 2, value: '足を運んで', done: true },
+    { id: 3, value: '不甲斐ない', done: true },
+    { id: 4, value: 'おろおろ', done: true },
+    { id: 5, value: '旅の恥はかき捨て', done: false },
+    { id: 6, value: '憤懣', done: false },
+    { id: 7, value: '溜飲', done: false },
+    { id: 8, value: '翻弄', done: false },
+  ]
+
+  return (
+    <ul className="pr-6">
+      {words.map(w => (
+        <SidebarItem key={w.id} word={w} isActive={activeId === w.id} />
+      ))}
+    </ul>
+  )
+}
+
+const SidebarItem = ({
+  word,
+  isActive,
+}: {
+  word: { id: number; value: string; done: boolean }
+  isActive: boolean
+}) => (
+  <li className="mb-1">
+    <a
+      className={classNames(
+        'px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block font-medium',
+        {
+          'text-purple-600': isActive,
+          'hover:translate-x-2px hover:text-gray-900 text-gray-600': !isActive,
+        },
+      )}
+      href="#"
+    >
+      <span
+        className={classNames('rounded absolute inset-0 bg-purple-200', {
+          'opacity-25': isActive,
+          'opacity-0': !isActive,
+        })}
+      ></span>
+      <span className="relative">{word.value}</span>
+    </a>
+  </li>
+)
+
+const CardWizard = () => null
