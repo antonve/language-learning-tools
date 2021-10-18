54e6315183894a9b9a7f41f9f2fac346c4e8cdd8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-10-18 13:24:29 +0900

    configure tailwindcss

--- frontend/anki/pages/index.tsx
+++ frontend/anki/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
-  return <div></div>
+  return <div className="bg-gray-100">test</div>
 }
 
 export default Home
