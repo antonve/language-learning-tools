8ea4446cde2547a9e01e21eec1fba8c647dde73b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-10-24 13:02:19 +0900

    fill japanese definition

--- frontend/anki/src/app/hooks.ts
+++ frontend/anki/src/app/hooks.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import { formatDefinitions, SentencesResult } from '@app/domain'
-import { getJishoDefinition, getSentences } from '@app/api'
+import { getGooDefinition, getJishoDefinition, getSentences } from '@app/api'
 
 export const useSentences = (word: string | undefined) => {
   const [sentences, setSentences] = useState(
@@ -24,7 +24,7 @@ export const useSentences = (word: string | undefined) => {
   }
 }
 
-interface DefinitionResult {
+interface EnglishDefinitionResult {
   word: string
   definition: string | undefined
   finished: boolean
@@ -32,7 +32,7 @@ interface DefinitionResult {
 
 export const useEnglishDefition = (word: string | undefined) => {
   const [definition, setDefinition] = useState(
-    undefined as DefinitionResult | undefined,
+    undefined as EnglishDefinitionResult | undefined,
   )
 
   useEffect(() => {
@@ -54,6 +54,50 @@ export const useEnglishDefition = (word: string | undefined) => {
       setDefinition({
         word,
         definition: def,
+        finished: true,
+      })
+    }
+
+    update()
+  }, [word])
+
+  return {
+    definition,
+  }
+}
+
+interface JapaneseDefinitionResult {
+  word: string
+  definition: string | undefined
+  reading: string | undefined
+  finished: boolean
+}
+
+export const useJapaneseDefition = (word: string | undefined) => {
+  const [definition, setDefinition] = useState(
+    undefined as JapaneseDefinitionResult | undefined,
+  )
+
+  useEffect(() => {
+    if (word === undefined) {
+      setDefinition(undefined)
+      return
+    }
+
+    setDefinition({
+      word,
+      definition: undefined,
+      reading: undefined,
+      finished: false,
+    })
+
+    const update = async () => {
+      const req = await getGooDefinition(word)
+
+      setDefinition({
+        word,
+        definition: req.definition,
+        reading: req.reading,
         finished: true,
       })
     }
