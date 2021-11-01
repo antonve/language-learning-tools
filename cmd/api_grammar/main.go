1dd08606fcc61af0126701cb06a208b36d2ef3b7
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-11-01 19:24:49 +0900

    add stub for api grammar

---
+++ cmd/api_grammar/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.Logger.Fatal(e.Start(":5555"))
+}
