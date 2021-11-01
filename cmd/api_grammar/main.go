af0c4fd532fb8c041937cdc466080372912c9261
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-11-01 19:44:11 +0900

    add server deps for grammar api

--- cmd/api_grammar/main.go
+++ cmd/api_grammar/main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"net/http"
 
+	"github.com/creasty/configo"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type ServerDependencies interface {
+	AutoConfigure() error
+
+	Init()
+}
+
+func NewServerDependencies() ServerDependencies {
+	return &serverDependencies{}
+}
+
+type serverDependencies struct {
+	Environment          string   `envconfig:"app_env" valid:"required" default:"development"`
+	DatabaseURL          string   `envconfig:"database_url" valid:"required"`
+	DatabaseMaxIdleConns int      `envconfig:"database_max_idle_conns" valid:"required"`
+	DatabaseMaxOpenConns int      `envconfig:"database_max_open_conns" valid:"required"`
+	CORSAllowedOrigins   []string `envconfig:"cors_allowed_origins" valid:"required"`
+	Port                 string   `envconfig:"app_port" valid:"required"`
+}
+
+func (d *serverDependencies) AutoConfigure() error {
+	return configo.Load(d, configo.Option{})
+}
+
+func (d *serverDependencies) Init() {
+}
 
 func main() {
 	e := echo.New()
