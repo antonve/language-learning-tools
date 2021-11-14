e207501fb16ba95dc740c1c3001c4d652574c9c6
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-11-14 20:38:43 +0900

    show random question

--- frontend/grammarquiz/pages/index.tsx
+++ frontend/grammarquiz/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState, Fragment } from 'react'
 import type { NextPage } from 'next'
 import getConfig from 'next/config'
 
@@ -18,6 +19,29 @@ interface Props {
   questions: Question[]
 }
 
+const useQuestion = (questions: Question[]) => {
+  const [question, setQuestion] = useState(() => randomValue(questions))
+  const answers = {
+    1: question.a,
+    2: question.b,
+    3: question.c,
+    4: question.d,
+  }
+
+  return {
+    question: question.sentence,
+    answers,
+    check: (answer: number) => answer.toString() === question.answer,
+    next: () => {
+      setQuestion(randomValue(questions))
+    },
+  }
+}
+
+const randomValue = <T extends unknown>(arr: T[]): T => {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
 const Home: NextPage<Props> = ({ questions }) => {
   return (
     <div className="mb-8 bg-gray-900 h-screen">
@@ -28,8 +52,41 @@ const Home: NextPage<Props> = ({ questions }) => {
           Grammar Quiz
         </h1>
       </header>
-      <div className="px-10"></div>
+      <div className="px-10">
+        <Question questions={questions} />
+      </div>
     </div>
+  )
+}
+
+const Question = ({ questions }: { questions: Question[] }) => {
+  const { question, answers, check, next } = useQuestion(questions)
+
+  return (
+    <div>
+      <div className="w-100 bg-gray-800 rounded-xl p-8 text-gray-300 text-4xl leading-relaxed">
+        <QuestionView sentence={question} />
+      </div>
+    </div>
+  )
+}
+
+const QuestionView = ({ sentence }: { sentence: string }) => {
+  const parts = sentence.split('＊')
+
+  return (
+    <>
+      {parts.map((str, i) => (
+        <Fragment key={i}>
+          {str}
+          {i < parts.length - 1 && (
+            <span className="opacity-50 bg-blue-300 py-0 w-24 mx-4 rounded-lg inline-block leading-none">
+              &nbsp;
+            </span>
+          )}
+        </Fragment>
+      ))}
+    </>
   )
 }
 
