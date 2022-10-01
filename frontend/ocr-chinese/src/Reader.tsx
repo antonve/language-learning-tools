5278c818dfaff1ba84c9021af3da99f8426881a8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-01 18:27:32 +0900

    load file in memory

--- frontend/ocr-chinese/src/Reader.tsx
+++ frontend/ocr-chinese/src/Reader.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 interface Book {}
 
 interface Props {
@@ -13,9 +15,29 @@ const Reader = ({ book }: Props) => {
 }
 
 const BookImporter = () => {
+  const [book, setBook] = useState<any>()
+
+  useEffect(() => {
+    if (!book) {
+      return
+    }
+
+    console.log(book)
+    const reader = new FileReader()
+    // reader.onload = e => console.log('loaded', e.target?.result)
+    reader.readAsDataURL(book)
+  }, [book])
+
   return (
-    <div className="border-4 border-indigo-400 border-dashed p-16 m-16 text-indigo-400 opacity-6 text-4xl text-center hover:opacity-50 cursor-move">
+    <div className="border-4 border-indigo-400 border-dashed p-16 m-16 text-indigo-400 opacity-6 text-4xl text-center hover:opacity-50 cursor-move relative">
       Drag book to import
+      <input
+        type="file"
+        className="absolute left-0 right-0 top-0 bottom-0 opacity-0"
+        onChange={e => {
+          setBook(e.target.files?.[0])
+        }}
+      />
     </div>
   )
 }
