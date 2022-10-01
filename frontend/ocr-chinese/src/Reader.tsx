a5d2f8ff103dc52fd69b2335b3a66b7d4fb067bb
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-01 18:44:16 +0900

    load file metadata in memory

--- frontend/ocr-chinese/src/Reader.tsx
+++ frontend/ocr-chinese/src/Reader.tsx
@@ -1,21 +1,9 @@
 import { useEffect, useState } from 'react'
 
-interface Book {}
+interface Props {}
 
-interface Props {
-  book: Book | undefined
-}
-
-const Reader = ({ book }: Props) => {
-  if (!book) {
-    return <BookImporter />
-  }
-
-  return <>Show book reader</>
-}
-
-const BookImporter = () => {
-  const [book, setBook] = useState<any>()
+const Reader = ({}: Props) => {
+  const [book, setBook] = useState()
 
   useEffect(() => {
     if (!book) {
@@ -23,17 +11,26 @@ const BookImporter = () => {
     }
 
     console.log(book)
-    const reader = new FileReader()
-    // reader.onload = e => console.log('loaded', e.target?.result)
-    reader.readAsDataURL(book)
+    // const reader = new FileReader()
+    // reader.onload = e => console.log('loaded', e.target)
+    // reader.readAsDataURL(book)
   }, [book])
 
+  if (!book) {
+    return <BookImporter setBook={setBook} />
+  }
+
+  return <>Show book reader</>
+}
+
+const BookImporter = ({ setBook }: { setBook: (book: any) => void }) => {
   return (
     <div className="border-4 border-indigo-400 border-dashed p-16 m-16 text-indigo-400 opacity-6 text-4xl text-center hover:opacity-50 cursor-move relative">
       Drag book to import
       <input
         type="file"
         className="absolute left-0 right-0 top-0 bottom-0 opacity-0"
+        accept=".epub"
         onChange={e => {
           setBook(e.target.files?.[0])
         }}
