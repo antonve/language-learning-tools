72ed9ec004d3e0bdb95d2c709a8b35370a191300
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-17 20:35:14 +0900

    fetch all images from epub in order

--- cmd/extract_epub/main.go
+++ cmd/extract_epub/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"strings"
 
+	"github.com/antchfx/htmlquery"
 	"github.com/kapmahc/epub"
 )
 
@@ -26,24 +27,70 @@ func main() {
 	//   fmt.Println(f)
 	// }
 
-	pages := getHTMLPages(book)
+	pageURLs := getPageList(book)
+	// fmt.Println(pageURLs)
+	pageReaders := getPageReaders(book, pageURLs)
+	// fmt.Println(pageReaders)
 
-	page := strings.TrimPrefix(pages[0], "../")
+	images := getImages(book, pageReaders)
 
-	r, err := book.Open(page)
-	if err != nil {
-		panic(err)
+	fmt.Println(len(images))
+
+	for _, image := range images {
+		fmt.Println(image)
 	}
-
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(data))
 }
 
-func getHTMLPages(book *epub.Book) []string {
+func getImages(book *epub.Book, readers []io.Reader) []string {
+	res := []string{}
+
+	for _, r := range readers {
+		for _, url := range getImageURLs(r) {
+			res = append(res, url)
+		}
+	}
+
+	return res
+}
+
+func getImageURLs(page io.Reader) []string {
+	urls := []string{}
+
+	doc, err := htmlquery.Parse(page)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, img := range htmlquery.Find(doc, "//img") {
+		url := trimURL(htmlquery.SelectAttr(img, "src"))
+		urls = append(urls, url)
+	}
+
+	return urls
+}
+
+func trimURL(url string) string {
+	return strings.TrimPrefix(url, "../")
+}
+
+func getPageReaders(book *epub.Book, pages []string) []io.Reader {
+	res := []io.Reader{}
+
+	for _, page := range pages {
+		p := trimURL(page)
+
+		r, err := book.Open(p)
+		if err != nil {
+			panic(err)
+		}
+
+		res = append(res, r)
+	}
+
+	return res
+}
+
+func getPageList(book *epub.Book) []string {
 	points := book.Ncx.Points
 
 	res := make([]string, len(points))
