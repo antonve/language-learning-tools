a77a2cafa0cd64df936348785f1e76c433f712b2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-17 16:31:16 +0900

    read all html pages in order from epub

--- cmd/extract_epub/main.go
+++ cmd/extract_epub/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
+
+	"github.com/kapmahc/epub"
 )
 
 func main() {
@@ -10,7 +14,42 @@ func main() {
 		fmt.Println("missing epub file as argument")
 		os.Exit(1)
 	}
+
 	path := os.Args[1]
 
-	fmt.Println(path)
+	book, err := epub.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	// for _, f := range book.Files() {
+	//   fmt.Println(f)
+	// }
+
+	pages := getHTMLPages(book)
+
+	page := strings.TrimPrefix(pages[0], "../")
+
+	r, err := book.Open(page)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+}
+
+func getHTMLPages(book *epub.Book) []string {
+	points := book.Ncx.Points
+
+	res := make([]string, len(points))
+	for i, p := range points {
+		res[i] = p.Content.Src
+	}
+
+	return res
 }
