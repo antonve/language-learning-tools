808c744ebbe7a9c4aa977d939d3c83e95a316696
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-23 21:38:38 +0900

    fix find_sentence

--- cmd/find_sentence/main.go
+++ cmd/find_sentence/main.go
@@ -10,13 +10,15 @@ import (
 func main() {
 	var word string
 	var path string
+	var language string
 
 	flag.StringVar(&word, "word", "です", "the word to search for")
 	flag.StringVar(&path, "path", "./out", "the path in which to look for files")
+	flag.StringVar(&language, "language", "ja", "the language of the corpus to search")
 
 	flag.Parse()
 
-	corpus, err := corpus.New(path)
+	corpus, err := corpus.New(path, language)
 	if err != nil {
 		panic(err)
 	}
