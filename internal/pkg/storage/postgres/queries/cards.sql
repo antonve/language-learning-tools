a0e6cc2b67b20939796288a82607f4883aadd339
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-24 17:46:04 +0900

    initial queries to interact with pending cards

---
+++ internal/pkg/storage/postgres/queries/cards.sql
@@ -0,0 +1,42 @@
+-- name: ListPendingCards :many
+select
+  id,
+  language_code,
+  token,
+  source_image,
+  meta,
+  created_at,
+  updated_at
+from pending_cards
+where
+  exported_at is null
+  and language_code = sqlc.arg('language_code')
+order by created_at asc;
+
+-- name: CreatePendingCards :one
+insert into pending_cards (
+  language_code,
+  token,
+  source_image,
+  meta
+) values (
+  sqlc.arg('language_code'),
+  sqlc.arg('token'),
+  sqlc.arg('source_image'),
+  sqlc.arg('meta')
+)
+returning id;
+
+-- name: UpdateCard :exec
+update pending_cards
+set
+  meta = sqlc.arg('meta'),
+  updated_at = now()
+where id = sqlc.arg('id');
+
+-- name: MarkCardAsExported :exec
+update pending_cards
+set
+  updated_at = now(),
+  exported_at = now()
+where id = sqlc.arg('id');
