98b30925108e631f91433ecd6dc4ec6947ea49fe
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-26 20:11:09 +0900

    configure base path for ocr chinese frontend

--- frontend/ocr-chinese/next.config.js
+++ frontend/ocr-chinese/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: "/miner/ocr-chinese",
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(epub)$/i,
