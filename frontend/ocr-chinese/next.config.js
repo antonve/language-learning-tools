829a9a124b747ec4a3c2e59a02dd7f71353e0597
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-26 22:45:33 +0900

    upgrade next

--- frontend/ocr-chinese/next.config.js
+++ frontend/ocr-chinese/next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: "/miner/ocr-chinese",
+  basePath: '/miner/ocr-chinese',
   output: 'standalone',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(epub)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/[hash][ext]',
-      },
-    })
-    return config
-  },
 }
