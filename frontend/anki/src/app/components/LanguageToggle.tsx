d4c81005fc8057af3b8b213ccc4035ff1785d577
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-10-29 19:46:20 +0900

    fix key issue language toggle

--- frontend/anki/src/app/components/LanguageToggle.tsx
+++ frontend/anki/src/app/components/LanguageToggle.tsx
@@ -6,21 +6,21 @@ interface Props {
   languages: string[]
 }
 
-const LanguageToggle = ({ selectedLanguage, languages}: Props) => (
-  <div className='w-full max-w-md px-2 sm:px-0 bg-red-200'>
+const LanguageToggle = ({ selectedLanguage, languages }: Props) => (
+  <div className="w-full max-w-md px-2 sm:px-0 bg-red-200">
     <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
-      {languages.map((lang) => (
-        <Tab as={Fragment}>
-            {({ selected }) => (
-              <button
-                className={
-                  selected ? 'bg-blue-500 text-white' : 'bg-white text-black'
-                }
-              >
-                {lang}
-              </button>
-            )}
-          </Tab>
+      {languages.map(lang => (
+        <Tab as={Fragment} key={lang}>
+          {({ selected }) => (
+            <button
+              className={
+                selected ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }
+            >
+              {lang}
+            </button>
+          )}
+        </Tab>
       ))}
     </Tab.List>
   </div>
