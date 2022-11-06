b6d8e2ecd7419d1fda7756afab8c4143662bb0e8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-11-06 16:57:40 +0900

    add nav bar to prepare for merging frontends

--- frontend/anki/src/app/components/Layout.tsx
+++ frontend/anki/src/app/components/Layout.tsx
@@ -6,16 +6,36 @@ interface Props {
 
 const Layout: FC<Props> = ({ children, navigation }) => (
   <div className="mb-8">
+    <div className="flex bg-gray-100">
+      <NavLink href="/anki">Anki Miner</NavLink>
+      <NavLink href="/manga-reader">Manga Reader</NavLink>
+      <NavLink href="/text-reader">Text Reader</NavLink>
+    </div>
     <header
-      className={`bg-grey-lightest border-t-4 border-purple-400 hero-background p-10 flex justify-between`}
+      className={`bg-grey-lightest hero-background p-10 flex justify-between`}
     >
       <h1 className="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
-        Anki Miner
+        Language Tools
       </h1>
       {navigation && navigation()}
     </header>
     <div className="px-10">{children}</div>
   </div>
+)
+
+const NavLink = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => (
+  <a
+    href={href}
+    className="px-6 py-1 font-semibold text-xs hover:bg-gray-600 hover:text-white"
+  >
+    {children}
+  </a>
 )
 
 export default Layout
