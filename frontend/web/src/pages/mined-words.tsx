90a6d2d78344d7ab217272ec98a1c40c5f712fda
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-11-07 20:42:15 +0900

    list up all pending cards

--- frontend/web/src/pages/mined-words.tsx
+++ frontend/web/src/pages/mined-words.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import Layout from '@app/Layout'
+import { getPendingCards, PendingCardsResponse } from '@app/anki/api'
 
 const MinedWords: NextPage<{}> = () => {
-  return null
+  const [words, setWords] = useState<PendingCardsResponse['cards']>([])
+
+  useEffect(() => {
+    getPendingCards('zho').then(res => {
+      setWords(res.cards)
+    })
+  }, [])
+
+  return (
+    <Layout
+      navigation={() => (
+        <h1 className="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
+          Mined Words Manager
+        </h1>
+      )}
+    >
+      <ul>
+        {words.map(w => (
+          <li key={w.id}>{w.token}</li>
+        ))}
+      </ul>
+    </Layout>
+  )
 }
 
 export default MinedWords
