0d375a2433fa07df1f6f633898bfd5c0e3700a8c
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-11-08 19:42:25 +0900

    remove hanzi-tools since it's node only

--- frontend/web/src/app/chinesetextreader/Reader.tsx
+++ frontend/web/src/app/chinesetextreader/Reader.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
+import { segmentText } from '@app/chinesetextreader/domain'
 
 interface Props {
   text: string
 }
 
 const Reader: NextPage<Props> = ({ text }) => {
-  return <p>{text}</p>
+  return <p>{segmentText(text)}</p>
 }
 
 export default Reader
