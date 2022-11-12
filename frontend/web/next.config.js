059da649baf8e7de4a28aa40f5a964db5e9312d1
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-11-12 14:17:00 +0900

    update route for app

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -4,15 +4,15 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
-  assetPrefix: isProd ? 'https://tadoku.app/tools' : undefined,
+  assetPrefix: isProd ? 'https://tools.tadoku.app' : undefined,
   publicRuntimeConfig: {
-    API_ROOT: isProd ? 'https://tadoku.app/tools/api' : 'http://localhost:8080',
+    API_ROOT: isProd ? 'https://tools.tadoku.app/api' : 'http://localhost:8080',
   },
   async redirects() {
     return [
       {
         source: '/',
-        destination: isProd ? '/tools/anki/ja' : '/anki/ja',
+        destination: '/anki/ja',
         permanent: false,
       }
     ]
