27ed574e409d5689438f0265f94ed21b3f99b51b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2024-01-28 21:14:59 +0900

    support multiple results from lemma

--- cmd/german/lemma/main.go
+++ cmd/german/lemma/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aaaton/golem/v4"
 	"github.com/aaaton/golem/v4/dicts/de"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func main() {
@@ -21,6 +24,52 @@ func main() {
 
 	input := os.Args[1]
 
-	word := lemmatizer.Lemma(input)
-	fmt.Println(word)
+	results := []string{}
+
+	if found := lemmatizer.InDict(input); found {
+		words := lemmatizer.Lemmas(input)
+		results = append(results, words...)
+	}
+
+	inputWithEszett := strings.ReplaceAll(input, "ss", "ß")
+	if found := lemmatizer.InDict(inputWithEszett); found && inputWithEszett != input {
+		words := lemmatizer.Lemmas(inputWithEszett)
+		results = append(results, words...)
+	}
+
+	results = formatResults(results)
+
+	if len(results) == 0 {
+		fmt.Println("no lemma found")
+		return
+	}
+
+	fmt.Println(results)
+}
+
+var caser = cases.Title(language.German)
+
+func formatResults(results []string) []string {
+	newResults := map[string]bool{}
+
+	for _, word := range results {
+		if _, ok := newResults[word]; ok {
+			withCaps := caser.String(strings.ToLower(word))
+			if _, ok := newResults[withCaps]; !ok {
+				newResults[withCaps] = true
+			}
+		} else {
+			newResults[word] = true
+		}
+	}
+
+	keys := make([]string, len(newResults))
+
+	i := 0
+	for k := range newResults {
+		keys[i] = k
+		i++
+	}
+
+	return keys
 }
